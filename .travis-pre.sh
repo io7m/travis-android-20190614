225745b7f0743f07303af23f8ccf0b1a3c7f188e
Mark Raynsford <code@io7m.com>
2019-06-14 15:00:38 +0000

    Try calling sdkmanager

--- .travis-pre.sh
+++ .travis-pre.sh
@@ -5,3 +5,7 @@ env | sort
 ls -alF /usr/local/android-sdk
 
 ls -alF /usr/local/android-sdk/tools/bin
+
+file /usr/local/android-sdk/tools/bin/sdkmanager
+
+/usr/local/android-sdk/tools/bin/sdkmanager
